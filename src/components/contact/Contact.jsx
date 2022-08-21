991bc216b9221878cb93802e81a9e4de3311963c
scarletknight13 <lamarbroxton@gmail.com>
2022-08-21 16:42:07 -0500

    fixed contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -24,7 +24,7 @@ function Contact() {
           <input type="text" name="name" placeholder='Your Full Name' required/>
           <input type="email" name="email" placeholder='Your Email' required/>
           <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
-          <button type='submit' className='btn btn-primary'>Send Message</button>
+          <button type='submit' className='send-btn btn btn-primary'>Send Message</button>
         </form>
       </div>
     </section>
